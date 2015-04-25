748cadd6d2b14729236c99fc8162826a4cc07599
Jose Galisteo <ceritium@gmail.com>
2015-04-25 02:37:02 +0200

    random

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
-var images = [];
-function loadImages(){
-  $.getJSON("http://api.giphy.com/v1/gifs/search?q=cats&api_key=dc6zaTOxFJmzC",
-    function(response){
-      $(response.data).each(function(index, item){
-        images.push(item.images.original.url);
-      });
-  });
-}
-
-loadImages();
 setInterval(function(){
-  if (images.lenght < 2) {
-  }
-  var img = images.shift();
-  $('body').attr('style', "background-image: url('" + img + "');");
+
+  $.getJSON("http://api.giphy.com/v1/gifs/random?tag=cats&rating=pg&api_key=dc6zaTOxFJmzC",
+    function(response){
+      var url = response.data.image_original_url;
+      $('body').attr('style', "background-image: url('" + url + "');");
+  });
+
+
 },6000);
 
 
